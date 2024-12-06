6d8e14576e7e0b28aa5f1216904ed505e2cc474d
TrikeMacatangay <trikemacatangay014@gmail.com>
2024-12-06 21:07:45 +0800

    Add files via upload

--- lab activity 4/capybara.py
+++
@@ -1,5 +0,0 @@
-class Capybara:
-    def __init__(self, name: str, gender: str, age: int):
-        self.name = name
-        self.gender = gender
-        self.age = age
--- lab activity 4/script.py
+++
@@ -1,9 +0,0 @@
-from capybara import Capybara
-
-def main():
-    test_case_number = int(input("Enter the test case number: "))
-    if test_case_number == 1:
-        capybara = Capybara("Biscoff", "M", 5)
-        print(f"Test Case {test_case_number}: Name: {capybara.name}, Gender: {capybara.gender}, Age: {capybara.age} years old")
-
-main()
